fef44700ea5dd1ff1462a4208bd3e6a24b080a94
ushka1 <david.prajzner01@gmail.com>
2024-03-15 22:26:57 +0100

    install mui

---
+++ src/config/theme.ts
@@ -0,0 +1,9 @@
+import createTheme from '@mui/material/styles/createTheme';
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: ['Roboto', 'sans-serif'].join(','),
+  },
+  palette: {},
+  components: {},
+});
