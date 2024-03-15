04a6dab2328c59b0b77fdb4bb1e98281e236ced8
ushka1 <david.prajzner01@gmail.com>
2024-03-15 23:41:31 +0100

    add zustand devtools

--- src/stores/useCatStore.ts
+++ src/stores/useCatStore.ts
@@ -1,11 +1,14 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
 interface CatState {
   cats: number;
   increase: (by: number) => void;
 }
 
-export const useCatStore = create<CatState>()((set) => ({
-  cats: 0,
-  increase: (by) => set((state) => ({ cats: state.cats + by })),
-}));
+export const useCatStore = create<CatState>()(
+  devtools((set) => ({
+    cats: 0,
+    increase: (by) => set((state) => ({ cats: state.cats + by })),
+  })),
+);
